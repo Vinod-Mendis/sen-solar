bcd271a9032934a22e123f7e99c9c5d1dd4b90c2
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-01-13 20:04:55 +0700

    implement button component

---
+++ src/app/components/ButtonComponent.jsx
@@ -0,0 +1,44 @@
+/** @format */
+"use client";
+
+import React from "react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import map_icon from "../../../public/icons/map-pin.svg";
+import circleArrowGreen from "../../../public/icons/circleArrowGreen.svg";
+import circleArrowWhite from "../../../public/icons/circleArrowWhite.svg";
+import arrow from "../../../public/icons/arrow.svg";
+import download from "../../../public/icons/download.svg";
+
+export default function ButtonComponent({
+  variant,
+  icon,
+  size,
+  onClickFn,
+  children,
+}) {
+  const click = () => {
+    console.log("clicked");
+  };
+
+  const icons = {
+    location: map_icon,
+    circleArrow:
+      variant === "green_primary" ? circleArrowWhite : circleArrowGreen,
+    arrow: arrow,
+    download: download,
+  };
+
+  const iconVariant = icons[icon];
+
+  return (
+    <>
+      <Button variant={variant} onClick={click} size={size}>
+        {children}
+        {iconVariant && (
+          <Image src={iconVariant} width={20} height={20} alt="map-pin" />
+        )}
+      </Button>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import ButtonComponent from "./components/ButtonComponent";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="max-w-7xl mx-auto h-screen justify-center items-center flex ">
+      <ButtonComponent
+        variant={"green_outline"}
+        icon={"circleArrow"}
+        onClickFn={"submit"}>
+        Submit
+      </ButtonComponent>
+    </div>
+  );
 }
