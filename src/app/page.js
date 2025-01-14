7781009bb930be323b451d3434d8fca065e0d047
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-01-14 18:15:44 +0700

    implement benefit section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 /** @format */
 
+import Benefits from "./components/landing/Benefits";
 import Hero from "./components/landing/Hero";
 
 export default function Home() {
   return (
-    <div className="h-screen items-center flex flex-col">
+    <div className="items-center flex flex-col gap-20">
       <Hero />
+      <Benefits/>
     </div>
   );
 }
