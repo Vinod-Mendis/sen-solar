c3c021fe655141dbf3e9e056ba342f2e50f39ed6
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-01-23 10:06:39 +0700

    implement faq page

---
+++ src/app/faq/page.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import FAQ from "../components/landing/FAQ";
+
+export default function page() {
+  return (
+    <div className="pt-40 w-full max-w-7xl mx-auto">
+      <FAQ />
+    </div>
+  );
+}
